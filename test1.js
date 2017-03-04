e45d262959b148a4313f78db8021e8c41a3aebf4
Pablo Weber <pabloliweber@gmail.com>
2017-03-05 00:44:52 +0800

    Dish name and missing ingredients now in individual spans
    But still on same line

--- test1.js
+++ test1.js
@@ -12,19 +12,31 @@ function createHTML() {
 	var recipeDiv = [];
 	var title = [];
 	var ingredientsMissing = [];
+	var nameSpan = [];
+	var ingredientsMissingSpan = [];
 
 	for (var i = 0; i < recipes.length; i++) {
-		//Create a new div in each loop with and id of recipeDiv0, recipeDiv1... so you get a div for each recipe
+		//Create a new div in each loop with an id of recipeDiv0, recipeDiv1... so you get a div for each recipe
 		recipeDiv[i] = document.createElement("div");
 		recipeDiv[i].setAttribute("id", "recipeDiv" + i);
 		
-		//Get dish name and number of missing ingredients from API
+		//Create span with id of nameSpan0, nameSpan1... to contain name of dish; and create and append the actual name of dish to said span
+		nameSpan[i] = document.createElement("span");
+		nameSpan[i].setAttribute("id", "nameSpan" + i);
 		title[i] = document.createTextNode("Dish name: " + recipes[i].title);
-		ingredientsMissing[i] = document.createTextNode("Number of missing ingredients: " + recipes[i].missedIngredientCount)
+		nameSpan[i].appendChild(title[i])
 
-		//Append title and number of missing ingredients to their respective divs
-		recipeDiv[i].appendChild(title[i]);
-		recipeDiv[i].appendChild(ingredientsMissing[i]);
+		
+		//Create span with id of ingredientsMissingSpan0, ingredientsMissingSpan1... to contain number of missing ingredients; and create and append the actual number of missing ingredients to said span
+		ingredientsMissingSpan[i] = document.createElement("span");
+		ingredientsMissingSpan[i].setAttribute("id", "ingredientsMissingSpan" + i);
+		ingredientsMissing[i] = document.createTextNode("Number of missing ingredients: " + recipes[i].missedIngredientCount)
+		ingredientsMissingSpan[i].appendChild(ingredientsMissing[i])
+
+
+		//Append title and number of missing ingredients spans to main div
+		recipeDiv[i].appendChild(nameSpan[i]);
+		recipeDiv[i].appendChild(ingredientsMissingSpan[i]);
 		
 		//Append div to the html file so that it's visible
 		document.body.appendChild(recipeDiv[i]);
