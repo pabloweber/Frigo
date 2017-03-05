e66f7c4c66ce21d6f7ad1461828b6f6e5f52b3df
Pablo Weber <pabloliweber@gmail.com>
2017-03-05 16:51:36 +0800

    Images added and recipes appear as grid

--- test1.js
+++ test1.js
@@ -1,57 +1,73 @@
 console.log('JavaScript connected');
 
+function getRecipes(){
+	var inputVal = document.getElementById('ingredientsInput').value;
+	var upToIngredients = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=';
+	var afterIngredients = '&number=200&mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D'
 
-var frigo = new XMLHttpRequest();
-frigo.open("GET", "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=chicken&number=200&mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D", false);
-frigo.send();
+	var frigo = new XMLHttpRequest();
+	frigo.open("GET", upToIngredients + inputVal + afterIngredients, false);
+	frigo.send();
 
-var recipes = JSON.parse(frigo.response);
+	var recipes = JSON.parse(frigo.response);
 
-function createHTML() {
-	
-	var recipeDiv = [];
-	var title = [];
-	var ingredientsMissing = [];
-	var nameSpan = [];
-	var ingredientsMissingSpan = [];
-	// var linebreak = document.createElement("br");
-
-	for (var i = 0; i < recipes.length; i++) {
-		//Create a new div in each loop with an id of recipeDiv0, recipeDiv1... so you get a div for each recipe
-		recipeDiv[i] = document.createElement("div");
-		recipeDiv[i].setAttribute("id", "recipeDiv" + i);
+	function createHTML() {
 		
-		//Create span with id of nameSpan0, nameSpan1... to contain name of dish; and create and append the actual name of dish to said span
-		nameSpan[i] = document.createElement("span");
-		nameSpan[i].setAttribute("id", "nameSpan" + i);
-		title[i] = document.createTextNode("Dish name: " + recipes[i].title);
-		nameSpan[i].appendChild(title[i]);
-
-		
-		//Create span with id of ingredientsMissingSpan0, ingredientsMissingSpan1... to contain number of missing ingredients; and create and append the actual number of missing ingredients to said span
-		ingredientsMissingSpan[i] = document.createElement("span");
-		ingredientsMissingSpan[i].setAttribute("id", "ingredientsMissingSpan" + i);
-		ingredientsMissing[i] = document.createTextNode("Number of missing ingredients: " + recipes[i].missedIngredientCount);
-		ingredientsMissingSpan[i].appendChild(ingredientsMissing[i]);
+		var recipeDiv = [];
+		var title = [];
+		var ingredientsMissing = [];
+		var nameSpan = [];
+		var ingredientsMissingSpan = [];
+		var img = [];
 
 
-		//Append title and number of missing ingredients spans to main div separated by a linebreak
-		recipeDiv[i].appendChild(nameSpan[i]);
-		var linebreak = document.createElement("br");
-		recipeDiv[i].appendChild(linebreak);
-		recipeDiv[i].appendChild(ingredientsMissingSpan[i]);
+		for (var i = 0; i < recipes.length; i++) {
+			//Create a new div in each loop with an id of recipeDiv0, recipeDiv1... so you get a div for each recipe
+			recipeDiv[i] = document.createElement("div");
+			recipeDiv[i].setAttribute("id", "recipeDiv" + i);
 
-		
-		//Append div to the html file so that it's visible
-		document.body.appendChild(recipeDiv[i]);
+			
+			//Create span with id of nameSpan0, nameSpan1... to contain name of dish; and create and append the actual name of dish to said span
+			nameSpan[i] = document.createElement("span");
+			nameSpan[i].setAttribute("id", "nameSpan" + i);
+			title[i] = document.createTextNode("Dish name: " + recipes[i].title);
+			nameSpan[i].appendChild(title[i]);
+
+
+			//Create img tag with id of img0, im1... to contain image;
+			img[i] = document.createElement("img");
+			img[i].setAttribute("id", "img" + i);
+			img[i].setAttribute("src", recipes[i].image);
+
+			
+			//Create span with id of ingredientsMissingSpan0, ingredientsMissingSpan1... to contain number of missing ingredients; and create and append textNode containing the actual number of missing ingredients to said span
+			ingredientsMissingSpan[i] = document.createElement("span");
+			ingredientsMissingSpan[i].setAttribute("id", "ingredientsMissingSpan" + i);
+			ingredientsMissing[i] = document.createTextNode("Number of missing ingredients: " + recipes[i].missedIngredientCount);
+			ingredientsMissingSpan[i].appendChild(ingredientsMissing[i]);
+
+
+			//Append title and number of missing ingredients spans to main div separated by a linebreak
+			recipeDiv[i].appendChild(nameSpan[i]);
+			var linebreak = document.createElement("br");
+			recipeDiv[i].appendChild(linebreak);
+			recipeDiv[i].appendChild(img[i]);
+			recipeDiv[i].appendChild(ingredientsMissingSpan[i]);
+
+
+			
+			//Append div to the html file so that it's visible
+			document.body.appendChild(recipeDiv[i]);
+
+		}
+
+		console.log(recipes.length);
 
 	}
 
+	createHTML();
+
 }
-
-createHTML();
-
-
 
 console.log("JavaScript finished loading");
 
