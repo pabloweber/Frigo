86ae54bdce48cbba5d7aea60ea6544352c5f5b8a
Pablo Weber <pabloliweber@gmail.com>
2017-03-28 01:13:31 +0800

    Changed color
    New blue color with white text
    Rows of 4
    (+ auto scroll down on search)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getRecipes(){
 	var upToIngredients = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=';
 	var maxNumMissing = document.getElementById('maxNumMissing').value;
 
-	var afterIngredients = '&number=200&limitLicense=false&mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D'
+	var afterIngredients = '&number=100&mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D' //limitLicense=false&
 
 	var searchByIngredients = new XMLHttpRequest();
 	searchByIngredients.open("GET", upToIngredients + inputVal + afterIngredients, false);
@@ -13,94 +13,93 @@ function getRecipes(){
 
 	var recipes = JSON.parse(searchByIngredients.response);
 
-	function createHTML() {
-		
-		var recipeDiv = [];
-		var title = [];
-		var ingredientsMissing = [];
-		var nameSpan = [];
-		var ingredientsMissingSpan = [];
-		var img = [];
-		var descriptionSpan = [];
-		var descriptionText = [];
 
-		recipeContainer = document.createElement("div");
-		recipeContainer.setAttribute("class", "recipeContainer");
+	var recipeDiv = [];
+	var ingredientsMissing = [];
+	var name = [];
+	var img = [];
+	var description = [];
+	var descriptionText = [];
 
-		
+	recipeWrapper = document.createElement("div");
+	recipeWrapper.setAttribute("class", "recipeWrapper");
 
+	recipeContainer = document.createElement("div");
+	recipeContainer.setAttribute("class", "recipeContainer");
 
-		if (document.getElementsByClassName("recipeContainer") !== null) {
+	
+	if (document.getElementsByClassName("recipeWrapper") !== null) {
 
-			$(".recipeContainer").remove();
-
-		}
-
-		for (var i = 0; i < recipes.length; i++) {
-
-			// var getDescription = new XMLHttpRequest();
-			// var recId = recipes[i].id;
-			// getDescription.open("GET", "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + recId + "/summary?mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D", false);
-			// getDescription.send();
-
-			// var descriptions = JSON.parse(getDescription.response);
-
-			//Create a new div in each loop with an id of recipeDiv0, recipeDiv1... so you get a div for each recipe
-			recipeDiv[i] = document.createElement("div");
-			recipeDiv[i].setAttribute("id", "recipeDiv" + i);
-			recipeDiv[i].setAttribute("class", "recipeBlock");
-
-				
-			//Create span with id of nameSpan0, nameSpan1... to contain name of dish; and create and append the actual name of dish to said span
-			nameSpan[i] = document.createElement("span");
-			nameSpan[i].setAttribute("id", "nameSpan" + i);
-			title[i] = document.createTextNode(recipes[i].title);
-			nameSpan[i].appendChild(title[i]);
-
-
-			//Create img tag with id of img0, im1... to contain image;
-			img[i] = document.createElement("img");
-			img[i].setAttribute("id", "img" + i);
-			img[i].setAttribute("src", recipes[i].image);
-
-				
-			//Create span with id of ingredientsMissingSpan0, ingredientsMissingSpan1... to contain number of missing ingredients; and create and append textNode containing the actual number of missing ingredients to said span
-			ingredientsMissingSpan[i] = document.createElement("span");
-			ingredientsMissingSpan[i].setAttribute("id", "ingredientsMissingSpan" + i);
-			ingredientsMissing[i] = document.createTextNode("Number of missing ingredients: " + recipes[i].missedIngredientCount);
-			ingredientsMissingSpan[i].appendChild(ingredientsMissing[i]);
-
-
-			//Create span with id of descriptionSpan0, descriptionSpan1... to contain description; and create and append textNode containing the actual description to said span
-			// descriptionSpan[i] = document.createElement("span");
-			// descriptionSpan[i].setAttribute("id", "descriptionSpan" + i);
-			// descriptionText[i] = document.createTextNode(getDescription.summary);
-			// descriptionSpan[i].appendChild(descriptionText[i]);
-
-
-			//Append title and number of missing ingredients spans to main div separated by a linebreak
-			recipeDiv[i].appendChild(nameSpan[i]);
-			recipeDiv[i].appendChild(img[i]);
-			recipeDiv[i].appendChild(ingredientsMissingSpan[i]);
-			// recipeDiv[i].appendChild(descriptionSpan[i]);
-
-			recipeContainer.appendChild(recipeDiv[i]);
-		}
-
-		document.body.appendChild(recipeContainer);
-
-		$('.recipeContainer').imagesLoaded(function() {
-		    $('.recipeContainer').masonry({
-			  	itemSelector: 'div.recipeBlock',
-			  	columnWidth: 'div.recipeBlock'
-			});
-		});
-
-		console.log(recipes.length)
+		$(".recipeWrapper").remove();
 
 	}
 
-	createHTML();
+	for (var i = 0; i < recipes.length; i++) {
+
+		// var getDescription = new XMLHttpRequest();
+		// var recId = recipes[i].id;
+		// getDescription.open("GET", "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + recId + "/summary?mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D", false);
+		// getDescription.send();
+
+		// var descriptions = JSON.parse(getDescription.response);
+
+		//Create a new div in each loop with an id of recipeDiv0, recipeDiv1... so you get a div for each recipe
+		recipeDiv[i] = document.createElement("div");
+		recipeDiv[i].setAttribute("id", recipes[i].id);
+		recipeDiv[i].setAttribute("class", "recipeBlock");
+
+			
+		//Create span with id of nameSpan0, nameSpan1... to contain name of dish; and create and append the actual name of dish to said span
+		name[i] = document.createElement("span");
+		name[i].setAttribute("class", "name");
+		name[i].innerHTML = recipes[i].title;
+
+
+		//Create img tag with id of img0, im1... to contain image;
+		img[i] = document.createElement("img");
+		img[i].setAttribute("class", "img");
+		img[i].setAttribute("src", recipes[i].image);
+
+			
+		//Create span with id of ingredientsMissingSpan0, ingredientsMissingSpan1... to contain number of missing ingredients; and create and append textNode containing the actual number of missing ingredients to said span
+		ingredientsMissing[i] = document.createElement("div");
+		ingredientsMissing[i].setAttribute("class", "ingredientsMissing");
+		ingredientsMissing[i].innerHTML = "Number of missing ingredients: " + recipes[i].missedIngredientCount;
+
+
+		//Create span with id of description0, description1... to contain description; and create and append textNode containing the actual description to said span
+		// description[i] = document.createElement("div");
+		// description[i].setAttribute("class", "description");
+		// description[i].innerHTML = descriptions.summary;
+
+
+		//Append title and number of missing ingredients spans to main div separated by a linebreak
+		recipeDiv[i].appendChild(name[i]);
+		recipeDiv[i].appendChild(img[i]);
+		recipeDiv[i].appendChild(ingredientsMissing[i]);
+		// recipeDiv[i].appendChild(description[i]);
+
+		recipeContainer.appendChild(recipeDiv[i]);
+	}
+
+	
+	recipeWrapper.appendChild(recipeContainer);
+	document.body.appendChild(recipeWrapper);
+
+	$('.recipeWrapper').imagesLoaded(function() {
+	    $('.recipeWrapper').masonry({
+		  	itemSelector: 'div.recipeBlock',
+		  	columnWidth: 'div.recipeBlock'
+		});
+	});
+
+	console.log(recipes.length)
+
+
+	$('html,body').animate({
+       	scrollTop: $(".recipeContainer").offset().top
+    },'slow');
+
 }
 
 console.log("JavaScript finished loading");
