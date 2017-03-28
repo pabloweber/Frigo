048f7ba6342225fc987f7176cda23f93705dc6dc
Pablo Weber <pabloliweber@gmail.com>
2017-03-28 20:37:51 +0800

    Added favicon and links
    Recipes now clickable but with no href  / and favicon visible

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ console.log('JavaScript connected');
 function getRecipes(){
 	var inputVal = document.getElementById('ingredientsInput').value;
 	var upToIngredients = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=';
-	var maxNumMissing = document.getElementById('maxNumMissing').value;
-
-	var afterIngredients = '&number=100&mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D' //limitLicense=false&
+	var afterIngredients = '&number=200&limitLicense=false&fillIngredients=true&ranking=1&mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D' //limitLicense=false&
 
 	var searchByIngredients = new XMLHttpRequest();
 	searchByIngredients.open("GET", upToIngredients + inputVal + afterIngredients, false);
@@ -20,6 +18,7 @@ function getRecipes(){
 	var img = [];
 	var description = [];
 	var descriptionText = [];
+	var clickPart = [];
 
 	recipeWrapper = document.createElement("div");
 	recipeWrapper.setAttribute("class", "recipeWrapper");
@@ -60,11 +59,24 @@ function getRecipes(){
 		img[i].setAttribute("class", "img");
 		img[i].setAttribute("src", recipes[i].image);
 
-			
+		
 		//Create span with id of ingredientsMissingSpan0, ingredientsMissingSpan1... to contain number of missing ingredients; and create and append textNode containing the actual number of missing ingredients to said span
 		ingredientsMissing[i] = document.createElement("div");
 		ingredientsMissing[i].setAttribute("class", "ingredientsMissing");
-		ingredientsMissing[i].innerHTML = "Number of missing ingredients: " + recipes[i].missedIngredientCount;
+		ingredientsMissing[i].innerHTML = "<b>" + recipes[i].missedIngredients.length + " " + "missing ingredients:</b></br>";
+		var x = [];
+		for (var j = 0; j < recipes[i].missedIngredients.length; j++) {
+			x[j] = recipes[i].missedIngredients[j].name;
+			ingredientsMissing[i].append(x[j]);
+			if (j !== recipes[i].missedIngredients.length - 1) {
+				ingredientsMissing[i].append(", ");
+			}
+		}
+
+		//Create an a tag to make the block clickable
+		clickPart[i] = document.createElement("a");
+		clickPart[i].setAttribute("class", "clickPart");
+		clickPart[i].setAttribute("href", "http://www.google.com")
 
 
 		//Create span with id of description0, description1... to contain description; and create and append textNode containing the actual description to said span
@@ -74,6 +86,7 @@ function getRecipes(){
 
 
 		//Append title and number of missing ingredients spans to main div separated by a linebreak
+		recipeDiv[i].appendChild(clickPart[i]);
 		recipeDiv[i].appendChild(name[i]);
 		recipeDiv[i].appendChild(img[i]);
 		recipeDiv[i].appendChild(ingredientsMissing[i]);
@@ -91,14 +104,13 @@ function getRecipes(){
 		  	itemSelector: 'div.recipeBlock',
 		  	columnWidth: 'div.recipeBlock'
 		});
+
+		$('html,body').animate({
+       		scrollTop: $(".recipeContainer").offset().top
+    	},'slow');
 	});
 
 	console.log(recipes.length)
-
-
-	$('html,body').animate({
-       	scrollTop: $(".recipeContainer").offset().top
-    },'slow');
 
 }
 
