72b54087f2edad5811249d9f2c492e05e949677b
Pablo Weber <pabloliweber@gmail.com>
2017-04-17 18:19:12 +0800

    Added notes

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-console.log('JavaScript connected');
+function main(){
 
-function getRecipes(){
+	//Create API parameters based on ingredients inputed
 	var inputVal = document.getElementById('ingredientsInput').value;
 	var upToIngredients = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=';
-	var afterIngredients = '&number=200&limitLicense=false&fillIngredients=true&ranking=1&mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D' //limitLicense=false&
+	var afterIngredients = '&number=200&limitLicense=false&fillIngredients=true&ranking=1&limitLicense=false&mashape-key=0gbpW6Rs1ymshDLw1GaH2g0W8JOjp1x5kCQjsnPQ3FoiRkIu0D'
 
+	//Get recipes for specific ingredients from API 
 	var searchByIngredients = new XMLHttpRequest();
 	searchByIngredients.open("GET", upToIngredients + inputVal + afterIngredients, false);
 	searchByIngredients.send();
 
+	//Parse what API returns (in JSON)
 	var recipes = JSON.parse(searchByIngredients.response);
 
+	//Create arrays for HTML creation
 	var recipeDiv = [];
 	var ingredientsMissing = [];
 	var name = [];
@@ -19,6 +22,7 @@ function getRecipes(){
 	var descriptionText = [];
 	var clickPart = [];
 	var likes = [];
+	var search;
 
 	recipeWrapper = document.createElement("div");
 	recipeWrapper.setAttribute("class", "recipeWrapper");
@@ -26,36 +30,37 @@ function getRecipes(){
 	recipeContainer = document.createElement("div");
 	recipeContainer.setAttribute("class", "recipeContainer");
 
-	
+	//Delete visible recipes when re-search
 	if (document.getElementsByClassName("recipeWrapper") !== null) {
-
 		$(".recipeWrapper").remove();
-
 	}
 
+
+	//Create HTML
 	for (var i = 0; i < recipes.length; i++) {
 
-		//Create a new div in each loop with an id of recipeDiv0, recipeDiv1... so you get a div for each recipe
+		//Create divs so you get a div for each recipe
 		recipeDiv[i] = document.createElement("div");
 		recipeDiv[i].setAttribute("id", recipes[i].id);
 		recipeDiv[i].setAttribute("class", "recipeBlock");
 
 			
-		//Create span with id of nameSpan0, nameSpan1... to contain name of dish; and create and append the actual name of dish to said span
+		//Create spans to contain name of dish; and create and append the actual name of dish to said span
 		name[i] = document.createElement("span");
 		name[i].setAttribute("class", "name");
 		name[i].innerHTML = recipes[i].title;
 
 
-		//Create img tag with id of img0, im1... to contain image;
+		//Create img tags to contain image;
 		img[i] = document.createElement("img");
 		img[i].setAttribute("class", "img");
 		img[i].setAttribute("src", recipes[i].image);
 
 		
-		//Create span with id of ingredientsMissingSpan0, ingredientsMissingSpan1... to contain number of missing ingredients; and create and append textNode containing the actual number of missing ingredients to said span
+		//Create spans with id to contain number and names of missing ingredients
 		ingredientsMissing[i] = document.createElement("div");
 		ingredientsMissing[i].setAttribute("class", "ingredientsMissing");
+		//Add names of missing ingredients only if there are any
 		if (recipes[i].missedIngredients.length == 0) {
 			ingredientsMissing[i].innerHTML = "<b>You have all the ingredients!</b>";
 		} else {
@@ -70,60 +75,52 @@ function getRecipes(){
 			}
 		}
 
-
+		//Create divs to contain number of likes
 		likes[i] = document.createElement("div");
 		likes[i].setAttribute("class", "likes");
 		likes[i].innerHTML = "<b>Likes</b> " + recipes[i].likes;
 
-		//Create an a tag to make the block clickable
+		//Create anchor tags to make the recipe blocks clickable
 		// clickPart[i] = document.createElement("a");
 		// clickPart[i].setAttribute("class", "clickPart");
 		// clickPart[i].setAttribute("href", "recipePage.html")
 
 
-		//Create span with id of description0, description1... to contain description; and create and append textNode containing the actual description to said span
-		// description[i] = document.createElement("div");
-		// description[i].setAttribute("class", "description");
-		// description[i].innerHTML = descriptions.summary;
-
-
-		//Append title and number of missing ingredients spans to main div separated by a linebreak
+		//Append all parts of recipe blocks to recipe blocks
 		// recipeDiv[i].appendChild(clickPart[i]);
 		recipeDiv[i].appendChild(name[i]);
 		recipeDiv[i].appendChild(img[i]);
 		recipeDiv[i].appendChild(likes[i]);
 		recipeDiv[i].appendChild(ingredientsMissing[i]);
 
-
+		//Append recipe blocks to container
 		recipeContainer.appendChild(recipeDiv[i]);
 	}
 
 	
+	//Append recipe container to recipe wrapper and that to DOM (now recipes are visible)
 	recipeWrapper.appendChild(recipeContainer);
 	document.body.appendChild(recipeWrapper);
 
+	//Isotope initiation (grid format)
 	// $('.recipeWrapper').imagesLoaded(function() {
 	    $('.recipeWrapper').isotope({
 		  	itemSelector: 'div.recipeBlock',
 		  	columnWidth: 'div.recipeBlock'
 		});
 
+	    //Scroll down on when recipes ready
 		$('html,body').animate({
        		scrollTop: $(".recipeContainer").offset().top
     	},'slow');
 	// });
 
+
+	$(".recipeBlock").click(function() {
+		return $(this).attr('id');
+	})
+
 	console.log(recipes.length)
 
 
-	function getRecipeID() {
-		$('.recipeBlock').on('click', function () {
-			return this.id
-		});
-	}
-
 }
-
-
-
-console.log("JavaScript finished loading");
